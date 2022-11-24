ae4daaaa8f3813460442ab5b329c88f8c4bfa769
nebeltag <nebeltag@mail.ru>
2022-11-24 06:51:49 +0300

    homework_4

--- homework/src/components/Layout/Layout.jsx
+++ homework/src/components/Layout/Layout.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import MessageField from '../MessageField/MessageField';
+import MessageField from '../MessageField/MessageField.jsx';
+import ChatList from '../ChatList/ChatList.jsx';
+import Header from '../Header/Header.jsx'
+import './style.css';
+
+// import PropTypes from 'prop-types';
+
 
 export default class Layout extends Component {
+
+  // static propTypes = {
+  //   chatId: PropTypes.string
+  // }
+
+  // static defaultProps = {
+  //   chatId: '1'
+  // }
+
   render() {
+
+    let { chatId } = this.props;
+    let message = 'Chat Room';
+
     return (
-      <div>
-        <MessageField />
+      <div className='layout'>
+        <Header message={message} chatId={chatId} />
+        <div className='main'>
+          <MessageField />
+          <div className='chats'>
+            <ChatList />
+          </div>
+        </div>
       </div>
     )
   }
