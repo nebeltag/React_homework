a77f4c30ccd274aeef9a6b8563ee2f58b02abb47
nebeltag <nebeltag@mail.ru>
2022-11-25 23:32:21 +0300

    chat style updates

--- homework/src/components/Message/Message.jsx
+++ homework/src/components/Message/Message.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './style.css';
 import PropTypes from 'prop-types';
-import { Typography } from "@mui/material";
+import { Typography, Chip, Avatar, Stack } from "@mui/material";
 
 
 export default class Message extends React.Component {
@@ -11,16 +11,64 @@ export default class Message extends React.Component {
   };
 
   render() {
+    const message = this.props.sender !== 'Bot' ?
+
+      <Stack direction="row" spacing={1}
+        style={{
+          display: 'flex', flexDirection: 'column',
+          alignItems: "flex-end"
+        }}>
+        <Chip
+          avatar={<Avatar alt={this.props.sender}
+            src="/broken-image.jpg"
+          >
+          </Avatar>}
+          label={this.props.text}
+
+          color="primary"
+          className='sender'
+        />
+        {/* <span className="sender">{this.props.sender}</span>
+        <span className="text">{this.props.text}</span> */}
+      </Stack> :
+
+      <Stack direction="row" spacing={1}
+        style={{
+          display: 'flex', flexDirection: 'column',
+          alignItems: "flex-start"
+        }}>
+        <Chip
+          avatar={<Avatar alt={this.props.sender}
+            src="/broken-image.jpg"
+          >
+          </Avatar>}
+          label={this.props.text}
+
+          color="success"
+          className='text'
+        />
+      </Stack>
+    {/* <Typography style={{
+            marginBottom: '15px', display: 'flex', flexDirection: 'column',
+            alignItems: "flex-start"
+          }}>
+
+            <span className="sender">{this.props.sender}</span>
+            <span className="text">{this.props.text}</span>
+
+          </Typography> */}
 
     return (
+      <div>
+        {message}
+      </div>
 
+      // <Typography style={{ marginBottom: '15px', display: 'flex', flexDirection: 'column' }}>
 
-      <Typography style={{ marginBottom: '15px' }}>
+      //   <span className="sender">{this.props.sender}</span>
+      //   <span className="text">{this.props.text}</span>
 
-        <span className="sender">{this.props.sender}:</span>
-        <span className="text">{this.props.text}</span>
-
-      </Typography>
+      // </Typography>
 
 
 
